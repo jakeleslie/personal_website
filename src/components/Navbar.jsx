b19d35d00f74a9fc60cc1f50f3c5a512451a9c1d
Jake Leslie <jakeleslie221@gmail.com>
2023-02-20 16:08:18 -0500

    Navbar work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,32 +5,39 @@ import resume from '../../public/Resume.pdf'
 
 export default function Navbar() {
   return (
-    <ul>
-      <li>
-        <a href="/">Home</a>
-      </li>
-      <li>
-        <a href="/about">About</a>
-      </li>
-      <li>
-        <Link
-          href="/Resume.pdf"
-          alt="alt text"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Resume</Link>
+    // <ul>
+    //   <li>
+    //     <a href="/">Home</a>
+    //   </li>
+    //   <li>
+    //     <a href="/about">About</a>
+    //   </li>
+    //   <li>
+    //     <Link
+    //       href="/Resume.pdf"
+    //       alt="alt text"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >Resume</Link>
 
 
-      </li>
-      <li>
-        <Link href="https://github.com/jakeleslie">Github</Link>
-      </li>
-      <li>
-        <a href="/contact">Contact</a>
-      </li>
-      {/* <li>
-            <a href="#">Photos</a>
-          </li> */}
-    </ul>
+    //   </li>
+    //   <li>
+    //     <Link href="https://github.com/jakeleslie">Github</Link>
+    //   </li>
+    //   <li>
+    //     <a href="/contact">Contact</a>
+    //   </li>
+    //   {/* <li>
+    //         <a href="#">Photos</a>
+    //       </li> */}
+    // </ul>
+    <nav>
+      <Link href='/'>Home</Link>
+      <Link href='/about'>About</Link>
+      <Link href='/Resume.pdf'>Resume</Link>
+      <Link href='https://github.com/jakeleslie'>Github</Link>
+      <Link href='/contact'>Contact</Link>
+    </nav>
   )
 }
